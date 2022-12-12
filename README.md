b62e4e279316ff588a40901b66bf70568b9d19e0
E1P <ewan.pearce@gmail.com>
2022-12-12 10:42:51 +0000

    Set up basic server.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Express with Deta Demo Project
+
+NODE_ENV set as 'development' with start script.
+
+```bash
+NODE_ENV=development nodemon index.js
+```
+
+NODE_ENV set on server as 'production' via '.env' file.
+
+```bash
+deta update -e .env
+```
--- index.js
+++ index.js
@@ -1,9 +1,41 @@
 const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { Deta } = require("deta");
+const env = process.env.NODE_ENV;
+
+const projectKey = process.env.PROJECT_KEY;
+const deta = Deta(projectKey);
+const db = deta.Base("users");
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
 app.get("/", async (req, res) => {
-  res.send("Hello World");
+  res.send({ first_micro_api: "https://exuwbc.deta.dev/", NODE_ENV: env });
 });
+
+app.get("/users", async (req, res) => {
+  const data = await db.fetch({});
+  res.send(data);
+});
+
+app.get("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  const data = await db.get(id);
+
+  res.send(data);
+});
+
+app.post("/users", async (req, res) => {
+  const result = await db.put(req.body);
+  res.send(result);
+});
+
+if (env === "development") {
+  let port = process.env.PORT || 3000;
+  app.listen(port, () => console.log("Listening...", port));
+}
 
 module.exports = app;
